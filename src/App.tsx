a88601f48e38637c71e9f971572c94e68815d8f7
Akhilesh <akhilsharma3332@gmail.com>
2025-03-25 16:55:10 +0530

    [updating task completed. ]

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react' 
+import { useEffect, useState } from 'react' 
 import './App.css'
+import axios from 'axios'
+import {HOST} from "./constants"
+import { Task } from './model/task';
+import TaskCard from './components/task';
 
 function App() { 
-  const [taskList, setTaskList] = useState(["asdfads", 'asdfasdf']);
+  const [taskList, setTaskList] = useState<Task[]>([]);
+  
+  const fetch_all_tasks = async () => {
+
+    const response = await axios.get(`${HOST}/tasks`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem("authToken")}`,  
+      },
+    });
+
+    const result = await response.data; 
+    console.log(result);
+    setTaskList(result); 
+  }
+
+  useEffect(()=> {
+    fetch_all_tasks(); 
+  }, [])
 
   return (
-    <>
-      <h1>Hey</h1>
-      {taskList.map((ele, index) => (
-        <p className='' key={index}>{ele}</p>
+    <> 
+      {taskList.map((ele, index) => ( 
+          <TaskCard 
+              key={index}
+              id={ele.id} 
+              title={ele.title}
+              description={ele.description}
+              is_completed={ele.is_completed}
+            />
       ))}
     </>
   )
