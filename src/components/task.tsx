26831a21cdb9779de44a407dfd188058de56fb91
Akhilesh <akhilsharma3332@gmail.com>
2025-03-25 17:32:31 +0530

    [saving success], error showing success.

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios' 
 import { Task } from "../model/task"
 import { HOST } from '../constants';
+import { Checkbox } from '@material-tailwind/react';
 
 function TaskCard(props: Task) {  
 
@@ -10,7 +11,8 @@ function TaskCard(props: Task) {
   const [isEditing, setIsEditing] = useState<boolean>(false); 
   const [newIsCompleted, setNewIsCompleted] = useState<boolean>(props.is_completed); 
   const [isLoading, setIsLoading] = useState<boolean>(false); 
-  const [successMessage, setSuccessMessage] = useState<string>(); 
+  const [successMessage, setSuccessMessage] = useState<string>();  
+  const [errorMessage, setErrorMessage] = useState<string>();  
 
   useEffect(()=> { 
   }, [])
@@ -34,14 +36,13 @@ function TaskCard(props: Task) {
 
       if(response.status === 200){
         setSuccessMessage(result.message)
-      }else {
-        setSuccessMessage(result.message)
+      }else { 
+        setErrorMessage("Something Went Wrong"); 
       }
       
     } catch (error) {
       setIsLoading(false); 
-
-      alert("Something went wrong")
+      setErrorMessage("Something Went Wrong"); 
       console.log(error); 
     } 
   }
@@ -53,11 +54,16 @@ function TaskCard(props: Task) {
   return (
     <div className="max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-sm">
 
-          <button type="button" 
-                className="text-gray-800 bg-green-100 font-medium rounded-full text-sm px-3 me-2 mb-2">
-                  {newIsCompleted ? "completed" : "pending"}        
-          </button> 
-
+          <div className="flex items-center mb-4">
+              <input id="disabled-checkbox" type="checkbox" 
+                      value="" 
+                      checked={newIsCompleted}
+                      disabled={!isEditing}
+                      onChange={(e) => setNewIsCompleted(e.target.checked)}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+              <label htmlFor="disabled-checkbox" className="ms-2 text-gray-800 dark:text-gray-500">Completed</label>
+          </div>
+ 
           {isEditing ? (
               <input 
                   type="text" 
@@ -66,7 +72,7 @@ function TaskCard(props: Task) {
                   className="mb-1 w-full text-2xl text-bold px-2 py-1 border rounded-lg"
               />
           ) : (
-              <p className="mb-1 text-2xl font-semibold tracking-tight text-gray-900">{newTitle}</p>
+              <p className="mb-1 text-2xl tracking-tight text-gray-900">{newTitle}</p>
           )}
           {isEditing ? (
               <textarea
@@ -101,6 +107,47 @@ function TaskCard(props: Task) {
                 Edit
             </button> 
           )}  
+
+          {successMessage && 
+              <div className="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+              <svg className="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                {successMessage}
+              </div>
+              <button type="button" 
+                  onClick={() => setSuccessMessage(undefined)}
+                  className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                  <span className="sr-only">Close</span>
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+              </button>
+            </div>
+          } 
+
+
+        {errorMessage && 
+              <div className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                <svg className="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                {errorMessage}
+              </div>
+              <button type="button" 
+                  onClick={() => setErrorMessage(undefined)}
+                  className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                  <span className="sr-only">Close</span>
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+              </button>
+            </div>
+          } 
 
       </div>
   );
